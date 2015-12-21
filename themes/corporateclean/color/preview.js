f81472bdb69e77cb7a1b8e2c1cc4698d5c9b53f4
Sathish <sathish@amiciziatech.com>
2015-12-21 11:19:13 +0530

    -added new themes

--- themes/corporateclean/color/preview.js
+++ themes/corporateclean/color/preview.js
@@ -1,80 +1,84 @@
 
-(function ($) {
+(function ($, Drupal, drupalSettings) {
+
+  'use strict';
+
   Drupal.color = {
     logoChanged: false,
     callback: function(context, settings, form, farb, height, width) {
 		// Change the logo to be the real one.
 		if (!this.logoChanged) {
-		$('#preview #preview-logo img').attr('src', Drupal.settings.color.logo);
+      $('.color-preview .color-preview-logo img').attr('src', drupalSettings.color.logo);
 		this.logoChanged = true;
 		}
 		// Remove the logo if the setting is toggled off. 
-		if (Drupal.settings.color.logo == null) {
-		$('div').remove('#preview-logo');
-		}
-		
-		// Text preview.
-		$('#preview', form).css('color', $('#palette input[name="palette[base]"]', form).val());
-		$('#preview a', form).css('color', $('#palette input[name="palette[link]"]', form).val());
-		$('#preview-header-menu a', form).css('color', $('#palette input[name="palette[headermenulink]"]', form).val());
-		$('#preview-footer a', form).css('color', $('#palette input[name="palette[footerlink]"]', form).val());
-		$('#preview-footer-bottom a', form).css('color', $('#palette input[name="palette[footerlink]"]', form).val());
-		$('#preview-slogan', form).css('color', $('#palette input[name="palette[slogan]"]', form).val());
-		
-		// Headings.
-		var headingscolor = $('#palette input[name="palette[link]"]', form).val();
-		var headingsshadow = $('#palette input[name="palette[headingshadow]"]', form).val();
-		
-		$('#preview h1', form).attr('style', "color: " + headingscolor + "; text-shadow: 1px 1px 1px " + headingsshadow + ";");
-		
-		// Header.
-		var gradient_headertop = $('#palette input[name="palette[headertop]"]', form).val();
-		var gradient_headerbottom = $('#palette input[name="palette[headerbottom]"]', form).val();
-		
-		$('#preview #preview-header', form).attr('style', "background-color: " + gradient_headertop + "; background-image: -webkit-gradient(linear, 0% 0%, 0% 100%, from(" + gradient_headertop + "), to(" + gradient_headerbottom + ")); background-image: -moz-linear-gradient(-90deg, " + gradient_headertop + ", " + gradient_headerbottom + ");");
-		
-		// Header-menu.
-		$('#preview-header-menu', form).css('background-color', $('#palette input[name="palette[headermenu]"]', form).val());
-		$('#preview-header-menu', form).css('border-top-color', $('#palette input[name="palette[headermenuborder]"]', form).val());
-		$('#preview-header-menu', form).css('border-bottom-color', $('#palette input[name="palette[headermenuborder]"]', form).val());
-		
-		// Banner.
-		var gradient_bannertop = $('#palette input[name="palette[bannertop]"]', form).val();
-		var gradient_bannerbottom = $('#palette input[name="palette[bannerbottom]"]', form).val();
-		var bannerborder = $('#palette input[name="palette[bannerborder]"]', form).val();
-		
-		$('#preview #preview-banner', form).attr('style', "background-color: " + gradient_bannertop + "; background-image: -webkit-gradient(linear, 0% 0%, 0% 100%, from(" + gradient_bannertop + "), to(" + gradient_bannerbottom + ")); background-image: -moz-linear-gradient(-90deg, " + gradient_bannertop + ", " + gradient_bannerbottom + "); border-bottom: 1px solid " + bannerborder + ";");
-		
-		// Content.
-		var gradient_contenttop = $('#palette input[name="palette[contenttop]"]', form).val();
-		var gradient_contentbottom = $('#palette input[name="palette[contentbottom]"]', form).val();
-		
-		$('#preview #preview-content', form).attr('style', "background-color: " + gradient_contenttop + "; background-image: -webkit-gradient(linear, 0% 0%, 0% 100%, from(" + gradient_contenttop + "), to(" + gradient_contentbottom + ")); background-image: -moz-linear-gradient(-90deg, " + gradient_contenttop + ", " + gradient_contentbottom + ");");
-		
-		// Block.
-		var blockbg = $('#palette input[name="palette[blockbg]"]', form).val();
-		$('#preview .block', form).attr('style', "background-color: " + blockbg + ";");
-		
-		// Footer.
-		var gradient_footer = $('#palette input[name="palette[footer]"]', form).val();
-		$('#preview #preview-footer', form).attr('style', "background-color: " + gradient_footer + ";");
-		
-		// Footer bottom.
-		var gradient_footerbottomtop = $('#palette input[name="palette[footerbottomtop]"]', form).val();
-		var gradient_footerbottombottom = $('#palette input[name="palette[footerbottombottom]"]', form).val();
-		
-		$('#preview-footer-bottom', form).attr('style', "background-color: " + gradient_footerbottomtop + "; background-image: -webkit-gradient(linear, 0% 0%, 0% 100%, from(" + gradient_footerbottomtop + "), to(" + gradient_footerbottombottom + ")); background-image: -moz-linear-gradient(-90deg, " + gradient_footerbottomtop + ", " + gradient_footerbottombottom + ");");
-		$('#preview-footer-bottom', form).css('border-top-color', $('#palette input[name="palette[headermenuborder]"]', form).val());
-		
-		// Button.
-		var gradient_buttontop = $('#palette input[name="palette[buttontop]"]', form).val();
-		var gradient_buttonbottom = $('#palette input[name="palette[buttonbottom]"]', form).val();
-		var buttontext = $('#palette input[name="palette[buttontext]"]', form).val();
-		var buttontextshadow = $('#palette input[name="palette[buttontextshadow]"]', form).val();
-		var buttonboxshadow = $('#palette input[name="palette[buttonboxshadow]"]', form).val();
-		
-		$('#preview a.more', form).attr('style', "background-color: " + gradient_buttontop + "; background-image: -webkit-gradient(linear, 0% 0%, 0% 100%, from(" + gradient_buttontop + "), to(" + gradient_buttonbottom + ")); background-image: -moz-linear-gradient(-90deg, " + gradient_buttontop + ", " + gradient_buttonbottom + "); -webkit-box-shadow: 0px 1px 2px " + buttonboxshadow + "; -moz-box-shadow: 0px 1px 2px " + buttonboxshadow + "; box-shadow: 0px 1px 2px " + buttonboxshadow + "; text-shadow: 0 1px 1px " + buttontextshadow + "; color: " + buttontext + ";");
+      if (drupalSettings.color.logo === null) {
+        $('div').remove('.color-preview-logo');
+      }
+
+      // Text preview.
+
+      form.find('#preview').css('color', form.find('.js-color-palette input[name="palette[base]"]').val());
+      form.find('#preview a').css('color', form.find('.js-color-palette input[name="palette[link]"]').val());
+      form.find('#preview-header-menu a').css('color', form.find('.js-color-palette input[name="palette[headermenulink]"]').val());
+      form.find('#preview-footer a').css('color', form.find('.js-color-palette input[name="palette[footerlink]"]').val());
+      form.find('#preview-footer-bottom a').css('color', form.find('.js-color-palette input[name="palette[footerlink]"]').val());
+      form.find('#preview-slogan').css('color', form.find('.js-color-palette input[name="palette[slogan]"]').val());
+
+      // Headings.
+      var headingscolor = form.find('.js-color-palette input[name="palette[link]"]').val();
+      var headingsshadow = form.find('.js-color-palette input[name="palette[headingshadow]"]').val();
+
+      form.find('#preview h1').attr('style', "color: " + headingscolor + "; text-shadow: 1px 1px 1px " + headingsshadow + ";");
+
+      // Header.
+      var gradient_headertop = form.find('.js-color-palette input[name="palette[headertop]"]').val();
+      var gradient_headerbottom = form.find('.js-color-palette input[name="palette[headerbottom]"]').val();
+
+      form.find('#preview #preview-header').attr('style', "background-color: " + gradient_headertop + "; background-image: -webkit-gradient(linear, 0% 0%, 0% 100%, from(" + gradient_headertop + "), to(" + gradient_headerbottom + ")); background-image: -moz-linear-gradient(-90deg, " + gradient_headertop + ", " + gradient_headerbottom + ");");
+
+      // Header-menu.
+      form.find('#preview-header-menu').css('background-color', form.find('.js-color-palette input[name="palette[headermenu]"]').val());
+      form.find('#preview-header-menu').css('border-top-color', form.find('.js-color-palette input[name="palette[headermenuborder]"]').val());
+      form.find('#preview-header-menu').css('border-bottom-color', form.find('.js-color-palette input[name="palette[headermenuborder]"]').val());
+
+      // Banner.
+      var gradient_bannertop = form.find('.js-color-palette input[name="palette[bannertop]"]').val();
+      var gradient_bannerbottom = form.find('.js-color-palette input[name="palette[bannerbottom]"]').val();
+      var bannerborder = form.find('.js-color-palette input[name="palette[bannerborder]"]').val();
+
+      form.find('#preview #preview-banner').attr('style', "background-color: " + gradient_bannertop + "; background-image: -webkit-gradient(linear, 0% 0%, 0% 100%, from(" + gradient_bannertop + "), to(" + gradient_bannerbottom + ")); background-image: -moz-linear-gradient(-90deg, " + gradient_bannertop + ", " + gradient_bannerbottom + "); border-bottom: 1px solid " + bannerborder + ";");
+
+      // Content.
+      var gradient_contenttop = form.find('.js-color-palette input[name="palette[contenttop]"]').val();
+      var gradient_contentbottom = form.find('.js-color-palette input[name="palette[contentbottom]"]').val();
+
+      form.find('#preview #preview-content').attr('style', "background-color: " + gradient_contenttop + "; background-image: -webkit-gradient(linear, 0% 0%, 0% 100%, from(" + gradient_contenttop + "), to(" + gradient_contentbottom + ")); background-image: -moz-linear-gradient(-90deg, " + gradient_contenttop + ", " + gradient_contentbottom + ");");
+
+      // Block.
+      var blockbg = form.find('.js-color-palette input[name="palette[blockbg]"]').val();
+      form.find('#preview .block').attr('style', "background-color: " + blockbg + ";");
+
+      // Footer.
+      var gradient_footer = form.find('.js-color-palette input[name="palette[footer]"]').val();
+      form.find('.js-color-palette #preview-footer').attr('style', "background-color: " + gradient_footer + ";");
+
+      // Footer bottom.
+      var gradient_footerbottomtop = form.find('.js-color-palette input[name="palette[footerbottomtop]"]').val();
+      var gradient_footerbottombottom = form.find('.js-color-palette input[name="palette[footerbottombottom]"]').val();
+
+      form.find('#preview-footer-bottom').attr('style', "background-color: " + gradient_footerbottomtop + "; background-image: -webkit-gradient(linear, 0% 0%, 0% 100%, from(" + gradient_footerbottomtop + "), to(" + gradient_footerbottombottom + ")); background-image: -moz-linear-gradient(-90deg, " + gradient_footerbottomtop + ", " + gradient_footerbottombottom + ");");
+      form.find('#preview-footer-bottom').css('border-top-color', $('#palette input[name="palette[headermenuborder]"]', form).val());
+
+      // Button.
+      var gradient_buttontop = form.find('.js-color-palette input[name="palette[buttontop]"]').val();
+      var gradient_buttonbottom = form.find('.js-color-palette input[name="palette[buttonbottom]"]').val();
+      var buttontext = form.find('.js-color-palette input[name="palette[buttontext]"]').val();
+      var buttontextshadow = form.find('.js-color-palette input[name="palette[buttontextshadow]"]').val();
+      var buttonboxshadow = form.find('.js-color-palette input[name="palette[buttonboxshadow]"]').val();
+
+      form.find('#preview a.more').attr('style', "background-color: " + gradient_buttontop + "; background-image: -webkit-gradient(linear, 0% 0%, 0% 100%, from(" + gradient_buttontop + "), to(" + gradient_buttonbottom + ")); background-image: -moz-linear-gradient(-90deg, " + gradient_buttontop + ", " + gradient_buttonbottom + "); -webkit-box-shadow: 0px 1px 2px " + buttonboxshadow + "; -moz-box-shadow: 0px 1px 2px " + buttonboxshadow + "; box-shadow: 0px 1px 2px " + buttonboxshadow + "; text-shadow: 0 1px 1px " + buttontextshadow + "; color: " + buttontext + ";");
 
     }
   };
-})(jQuery);
+})(jQuery, Drupal, drupalSettings);
--- themes/corporateclean/theme-settings.php
+++ themes/corporateclean/theme-settings.php
@@ -150,7 +150,19 @@ function corporateclean_form_system_theme_settings_alter(&$form, &$form_state) {
 	'#default_value' => theme_get_setting('responsive_respond','corporateclean'),
     '#description'   => t('IE 6-8 require a JavaScript polyfill solution to add basic support of CSS3 media queries. Note that you should enable <strong>Aggregate and compress CSS files</strong> through <em>/admin/config/development/performance</em>.'),
   );
-    
+  $form['mtt_settings']['menu'] =array(
+    '#type' => 'fieldset',
+    '#title' => t('Menu'),
+    '#collapsible' => TRUE,
+    '#collapsed' => FALSE,
+  );
+  $form['mtt_settings']['menu']['menu_name'] = array(
+    '#type' => 'select',
+    '#title' => t('Menu list'),
+    '#options' => menu_ui_get_menus(),
+    '#default_value' => theme_get_setting('menu_name', 'corporateclean'),
+    '#description' => t('select the menu to display'),
+  );
   $form['mtt_settings']['credits'] = array(
     '#type' => 'fieldset',
     '#title' => t('Credits'),
